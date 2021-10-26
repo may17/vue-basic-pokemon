8cd5d63832997ba3ad3157153e734e1212a28d11
Joe Ray Gregory <joe@may17.de>
2021-10-26 22:47:51 +0200

    Add solution

--- scripts.js
+++ scripts.js
@@ -1,2 +1,45 @@
-// Vue code goes here
-const t = "ggg";
+Vue.createApp({
+  data() {
+    return {
+      pokemons: [
+        {
+          name: "Pikachu",
+          type: "electro",
+          hp: 35,
+          weight: 150,
+          height: 4,
+          visibleDetails: false,
+        },
+        {
+          name: "Joe",
+          type: "electro",
+          hp: 50,
+          weight: 180,
+          height: 80,
+          visibleDetails: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    showDetails(index) {
+      /* 
+            // Alternative solution 1
+            if (this.visibleDetails) {
+                this.visibleDetails = false
+            } else {
+                this.visibleDetails = true
+            }
+            
+            // Alternative solution 2
+            this.visibleDetails = this.visibleDetails
+                ? false
+                : true
+            */
+
+      // smartes solution
+      this.pokemons[index].visibleDetails =
+        !this.pokemons[index].visibleDetails;
+    },
+  },
+}).mount("#app");
