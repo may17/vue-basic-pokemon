6efee5f7cf94276f5e154c9698b97dc395ea815c
Joe Ray Gregory <joe@may17.de>
2021-10-27 12:31:21 +0200

    Add computed, created fetch and v-model

--- scripts.js
+++ scripts.js
@@ -1,30 +1,38 @@
 Vue.createApp({
   data() {
     return {
-      pokemons: [
-        {
-          name: "Pikachu",
-          type: "electro",
-          hp: 35,
-          weight: 150,
-          height: 4,
-          visibleDetails: false,
-        },
-        {
-          name: "Joe",
-          type: "electro",
-          hp: 50,
-          weight: 180,
-          height: 80,
-          visibleDetails: false,
-        },
-      ],
+      searchQuery: "",
+      pokemons: [],
     };
   },
+  computed: {
+    pokemonCount() {
+      return this.pokemonFilteredList.length;
+    },
+    pokemonFilteredList() {
+      return this.pokemons.filter((pokemon) =>
+        pokemon.name.startsWith(this.searchQuery)
+      );
+    },
+  },
   methods: {
+    filterItems() {
+      this.isFilterActive = true;
+    },
     showDetails(index) {
       this.pokemons[index].visibleDetails =
         !this.pokemons[index].visibleDetails;
     },
+    detailsButtonText(index) {
+      return this.pokemons[index].visibleDetails
+        ? "Hide Details"
+        : "Show Details";
+    },
+  },
+  async created() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
+    const jsonData = await response.json();
+
+    this.pokemons = jsonData.results;
   },
 }).mount("#app");
